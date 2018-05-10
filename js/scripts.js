8d26229fb16153f7fc301773cc5820f5c92ed958
Nastya Sneg <sneg.bz@gmail.com>
2018-05-11 00:04:32 +0300

    JS модальное окно, поправила фильтр

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,68 @@ slideToggle3.addEventListener("click", function(evt) {
   slideCurrent1.classList.remove("features-item-show");
   slideCurrent2.classList.remove("features-item-show");
 });
+
+var link = document.querySelector(".button-open-form");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var inputName = popup.querySelector("[name=name]");
+var inputEmail = popup.querySelector("[name=email]");
+var inputTextarea = popup.querySelector("[name=text]");
+
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+try {
+  storageName = localStorage.getItem("name");
+  storageEmail = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storageName) {
+    inputName.value = storageName;
+    inputEmail.focus();
+  } if (storageEmail) {
+    inputEmail.value = storageEmail;
+    inputTextarea.focus();
+  } else {
+    inputName.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!inputName.value || !inputEmail.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", inputName.value);
+      localStorage.setItem("email", inputEmail.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
